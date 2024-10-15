da8709a063b1919512d57689de9f99539485a524
mohammad <mohammadghiasi005@gmail.com>
2024-10-15 22:39:23 +0330

    adminChatbackend_v-1.1.4.7

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ app.use(express.json({ limit: '50mb' }));
 // * CORS Policy
 app.use(cors({
     origin: 'https://admin-chat-front.vercel.app', // Corrected URL
-    credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
--- utils/socketConnection.js
+++ utils/socketConnection.js
@@ -5,21 +5,10 @@ module.exports = (httpServer) => {
     const io = socketIo(httpServer, {
         cors: {
             origin: 'https://admin-chat-front.vercel.app', // Make sure this matches your frontend URL exactly (no trailing slashes)
-            methods: ['GET', 'POST'],
-            credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+            methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
         },
         transports: ['websocket', 'polling'], // Make sure websocket is allowed
     });
-
-    io.use((socket, next) => {
-        if (1) {
-            // code
-            next();
-        } else {
-            //code
-            next();
-        };
-    })
 
     return io;
 };
