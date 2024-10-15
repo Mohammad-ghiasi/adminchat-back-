cc1d7e39cf8237accd6596311799a2875697a52c
mohammad <mohammadghiasi005@gmail.com>
2024-10-15 22:28:07 +0330

    adminChatbackend_v-1.1.4.5

--- utils/socketConnection.js
+++ utils/socketConnection.js
@@ -4,9 +4,10 @@ const socketIo = require('socket.io');
 module.exports = (httpServer) => {
     const io = socketIo(httpServer, {
         cors: {
-            origin: 'https://admin-chat-front.vercel.app',
-            credentials: true,
-        },
+            origin: 'https://admin-chat-front.vercel.app', // Make sure this matches your frontend URL exactly (no trailing slashes)
+            methods: ['GET', 'POST'],
+            credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+        }
     });
 
     io.use((socket, next) => {
