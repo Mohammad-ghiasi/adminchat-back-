68fe05e6e7d397f1f3819141ba5e3b4512c4c9b5
mohammad <mohammadghiasi005@gmail.com>
2024-10-15 22:05:12 +0330

    adminChatbackend_v-1.1.4.3

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ app.use(express.json({ limit: '50mb' }));
 
 // * CORS Policy
 app.use(cors({
-    origin: 'https://admin-chat-front.vercel.app/', // Corrected URL
+    origin: 'https://admin-chat-front.vercel.app', // Corrected URL
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
