0a1718279af000383641a5b214afcc51168d49db
mohammad <mohammadghiasi005@gmail.com>
2024-10-15 22:00:54 +0330

    adminChatbackend_v-1.1.4.2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const chatRoute = require('./routes/chats');
 const userRoute = require('./routes/user');
-// const cors = require('cors');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
 const app = express();
