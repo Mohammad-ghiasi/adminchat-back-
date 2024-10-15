eb5a4e33ecf0a468805c9aa76de68002e302cb93
mohammad <mohammadghiasi005@gmail.com>
2024-10-15 21:07:16 +0330

    adminChatbackend_v-1.0.7

--- app.js
+++ app.js
@@ -1,36 +1,32 @@
 const express = require("express");
 const chatRoute = require('./routes/chats');
-const userRoute = require('./routes/user')
+const userRoute = require('./routes/user');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-
 
 const app = express();
 
 //* BodyParser
-app.use(express.urlencoded({ extended: true, limit: '50mb' }))
-app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json({ limit: '50mb' }));
 
 //* CORS Policy
-
-//* Template Engine
-
-// * static folder
-// app.use(express.static(path.join(__dirname, 'public')))
-
-// Middleware
 app.use(cors({
-    origin: 'https://admin-chat-front.vercel.app/', // Your frontend URL
-    credentials: true
+    origin: 'https://admin-chat-front.vercel.app', // Remove the trailing slash here
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Explicitly allow methods
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
 }));
-app.use(cookieParser());
 
+app.use(cookieParser());
 
 //* Routes
 app.use('/chat', chatRoute);
 app.use('/user', userRoute);
 
-
 //* 404 Error handler
+app.use((req, res, next) => {
+    res.status(404).send('Resource not found');
+});
 
 module.exports = app;
