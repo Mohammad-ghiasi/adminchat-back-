8ca0995368143331a8d4647356be255260f1f8eb
mohammad <mohammadghiasi005@gmail.com>
2024-10-15 21:59:13 +0330

    adminChatbackend_v-1.1.4.1

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ const app = express();
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.json({ limit: '50mb' }));
 
-//* CORS Policy
-// app.use(cors({
-//     origin: 'https://admin-chat-front.vercel.app', // Corrected URL
-//     credentials: true,
-//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-// }));
+// * CORS Policy
+app.use(cors({
+    origin: 'https://admin-chat-front.vercel.app/', // Corrected URL
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 
 app.use(cookieParser());
